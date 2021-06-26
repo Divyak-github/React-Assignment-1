3f4fe4ae23018611305a9fb62c3b49a4718320b5
Divyak-github <divya.kanakanala@gmail.com>
2021-06-26 20:29:30 +1000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
-import ExpenseItem from "./components/ExpenseItem";
-
-const dataList = [
-  {
-    expenseDate: new Date(2021, 6, 26),
-    expenseTitle: "Car Insurance",
-    expenseAmount: 12,
-  },
-  {
-    expenseDate: new Date(2021, 6, 26),
-    expenseTitle: "Car Insurance",
-    expenseAmount: 123,
-  },
-  {
-    expenseDate: new Date(2021, 6, 26),
-    expenseTitle: "Car Insurance",
-    expenseAmount: 1234,
-  },
-];
+import Expenses from "./components/Expenses";
 
 function App() {
-  return (
-    <div>
-      {dataList.map((data, index) => (
-        <ExpenseItem
-          expenseDate={data.expenseDate}
-          expenseTitle={data.expenseTitle}
-          expenseAmount={data.expenseAmount}
-        />
-      ))}
-    </div>
-  );
+  return <Expenses />;
 }
 
 export default App;
